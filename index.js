0c560e4d39d38e40db1aa9382d903002c0612e04
Green Kim <greenkimparsons@gmail.com>
2021-04-13 19:42:52 -0400

    got the prompts to show and object to show at the end

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const fs = require("fs");
-const render = require("./src/page-template.js");
+// const render = require("./src/page-template.js");
 
+const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
@@ -10,7 +11,7 @@ const jest = require("jest");
 
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-pageTemplate(team);
+// pageTemplate(team);
 
 // INDEX FILES ARE CONSIDERED THE ENTRY POINT TO YOUR APP
 // IF THIS IS YOUR ENTRY POINT, YOU MUST DO YOUR INQUIRER HERE
@@ -18,29 +19,28 @@ pageTemplate(team);
 const membersArray = [];
 
 function runApp() {
+  createAProfile();
   //   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-  const createAProfile = () => {
+  function createAProfile() {
     inquirer
       .prompt([
         {
           type: "list",
-          name: "employee type",
+          name: "employeeType",
           message: "I am creating a profile for?",
           choices: ["Engineer", "Intern", "Manager"],
         },
       ])
       .then((val) => {
-        // If the user says yes to another game, play again, otherwise quit the game
-        if (val.choices === "Engineer") {
+        if (val.employeeType === "Engineer") {
           createEngineer();
-        } else if (val.choices === "Intern") {
+        } else if (val.employeeType === "Intern") {
           createIntern();
-        } else if (val.choices === "Manager") {
+        } else if (val.employeeType === "Manager") {
           createManager();
-        } 
-
+        }
       });
-  };
+  }
 
   function createManager() {
     inquirer
@@ -48,50 +48,50 @@ function runApp() {
         {
           type: "input",
           name: "managerName",
-          message: "What is your manager's name?",
+          message: "Type name of manager.",
         },
         {
           type: "input",
           name: "managerID",
-          message: "What is your manager's id?",
+          message: "Type manager's ID.",
         },
         {
           type: "input",
           name: "managerEmail",
-          message: "What is your manager's email?",
+          message: "Type manager's email.",
         },
         {
           type: "input",
           name: "managerOfficeNum",
-          message: "What is your manager's office number?",
+          message: "Type manager's office number.",
         },
       ])
       .then((answers) => {
         //How can I store this info based on manager, engineer, and intern?
         // Where can I store this to call this info later on?
         console.log(answers);
-        const manager = new Manager(
-          answers.managerName,
-          answers.managerID,
-          answers.managerEmail,
-          answers.managerOfficeNum
-        );
-        console.log(manager);
-        membersArray.push(manager);
-        console.log(membersArray);
-        buildTeam();
+        // const manager = new Manager(
+        //   answers.managerName,
+        //   answers.managerID,
+        //   answers.managerEmail,
+        //   answers.managerOfficeNum
+        // );
+        // console.log(manager);
+        // membersArray.push(manager);
+        // console.log(membersArray);
+        // buildTeam();
       });
   }
 
-  function buildTeam() {
-    // Create the output directory if the output path doesn't exist
-    if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR); // creates a directory if it doesn't exist
-    }
-    // looking for path to dist folder
-    fs.writeFileSync(outputPath, render(membersArray), "utf-8");
-  }
-  buildTeam();
+  // function buildTeam() {
+  //   // Create the output directory if the output path doesn't exist
+  //   if (!fs.existsSync(OUTPUT_DIR)) {
+  //     fs.mkdirSync(OUTPUT_DIR); // creates a directory if it doesn't exist
+  //   }
+  //   // looking for path to dist folder
+  //   fs.writeFileSync(outputPath, render(membersArray), "utf-8");
+  // }
+  // buildTeam();
 }
 
 runApp();
